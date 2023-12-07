bdd460fda1158a155b119c0891c757aaf4ca83c7
Aarón Silvoso Garcia <93611856+alakinepepine@users.noreply.github.com>
2023-12-07 23:49:02 +0100

    feat: Add functionality to the code
    Now it is possible to parse the file location, which was the goal of this module.

--- xspf_parser.py
+++ xspf_parser.py
@@ -1,9 +1,16 @@
 import xml.etree.ElementTree as ET
 
 def parsear_archivo_xspf(archivo):
+    ET.register_namespace('', 'http://xspf.org/ns/0/')
+    
     tree = ET.parse(archivo)
     root = tree.getroot()
-    print(root)
+
+    tracks = root.find('.//{http://xspf.org/ns/0/}trackList')
+
+    for track in tracks.findall('{http://xspf.org/ns/0/}track'):
+        location = track.find('{http://xspf.org/ns/0/}location').text
+        print(f'Location: {location}')
 
 archivo_xspf = 'music.xspf'
 parsear_archivo_xspf(archivo_xspf)
