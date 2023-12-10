aefabd43a5457fdcecb4f994676510277c3af6b6
Aarón Silvoso Garcia <93611856+alakinepepine@users.noreply.github.com>
2023-12-10 13:48:34 +0100

    refactor: add dictionary requirements
    add dictionary requirements for the proyect

--- src/xspf_parser.py
+++ src/xspf_parser.py
@@ -2,26 +2,25 @@ import os
 import xml.etree.ElementTree as ET
 
 def xspf_parser(file_path):
-    
     if not os.path.isfile(file_path):
         print(f"The file: '{file_path}' doesn't exist.")
         return
     
     try:
-        ET.register_namespace('', 'http://xspf.org/ns/0/')
-        
         tree = ET.parse(file_path)
         root = tree.getroot()
         
-        track_list = root.find('.//{http://xspf.org/ns/0/}trackList')
+        namespace = {'xspf': 'http://xspf.org/ns/0/'}
+
+        track_list = root.find('.//xspf:trackList', namespace)
         if track_list is None:
             print("Error: The 'trackList' section was not found in the frontend.")
             return
 
         locations = []
-        for track in track_list.findall('{http://xspf.org/ns/0/}track'):
+        for track in track_list.findall('xspf:track', namespace):
             try:
-                location = track.find('{http://xspf.org/ns/0/}location').text
+                location = track.find('xspf:location', namespace).text
                 print(f'Location: {location}')
                 locations.append(location)
             except AttributeError:
